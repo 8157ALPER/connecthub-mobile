4b0ed4c7435fe9cc848ff42939bebb2f532907f1
8157ALPER <metealper@gmail.com>
2025-09-09 08:39:12 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.connecthub.app',
+  appId: 'com.connecthub.social',
   appName: 'ConnectHub',
   webDir: 'dist/public',
   server: {
